87d505fc84f8bb84c8415047694a3c9bcce5465f
Scott Anderson <scott@v1apps.com>
2018-08-28 14:43:36 -0400

    refactor to use actions object to drive path testing

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -45,6 +45,47 @@ class CustomPage {
     async getContentsOf(selector) {
         return this.page.$eval(selector, el => el.innerHTML)
     }
+
+    get(path) {
+        // note how we're using a second argument to pass in path and then referencing it 'in scope' via _path
+        return this.page.evaluate((_path) => {
+            return fetch(_path, {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then(res => res.json())
+        }, path)
+    }
+
+    post(path, data) {
+        return this.page.evaluate(
+            (_path, _data) => {
+                return fetch(_path, {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(_data)
+                }).then(res => res.json())
+            }, path, data)
+    }
+
+    // note how bracket notation is used to invoke the class function...
+    // also note use of destructuring to simplify the code
+    execRequests(actions) {
+        return Promise.all(
+            actions.map(({
+                method,
+                path,
+                data
+            }) => {
+                return this[method](path, data)
+            })
+        )
+    }
 }
 
 module.exports = CustomPage
